88f67d94e775bdd7e9a0049b9464d55c489fa846
Sathish Perumal <sathishperumalmsc@gmail.com>
2024-08-30 01:37:06 +0530

    Add project files.

---
+++ CheckoutProductSuite.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automation.SwagLabsNET.Test
+{
+    [TestFixture]
+    public class CheckoutProductSuite
+    {
+        IPage _page;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            _page = await GetPage.GetWebPage();
+        }
+
+        [Test]
+        public void Verify_CheckoutProduct_Workflow()
+        {
+            Assert.Pass();
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            await _page.CloseAsync();
+        }
+    }
+}
---
+++ GetPage.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Playwright;
+using Automation.SwagLabsNET.Pom;
+using Automation.SwagLabsNET.Extensions;
+using static System.Net.Mime.MediaTypeNames;
+
+using Automation.SwagLabsNET.Extensions.PlaywrightExtensions;
+using Automation.SwagLabsNET.Pom.Constants;
+
+namespace Automation.SwagLabsNET.Test
+{
+    public static class GetPage
+    {
+        private static IPage page;
+
+        public static async Task<IPage> GetWebPage() 
+        { 
+            var playwright = await Playwright.CreateAsync();
+            var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Channel = "chrome", Headless=false});
+            page = await browser.NewPageAsync();
+
+            await page.GotoAsync("https://www.saucedemo.com/");
+
+            await page.FillTextBoxByPlaceholderAsync(LoginConstants.UserNamePlaceholderText, LoginConstants.UserName);
+            await page.FillTextBoxByPlaceholderAsync(LoginConstants.PasswordPlaceholderText, LoginConstants.UserPassword);
+
+            await page.ClickbuttonByLocatorAsync(LoginConstants.LogInButtonId);
+
+            return page;
+        }
+    }
+}
