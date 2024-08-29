70dc1cc5233d9f027c32a60ca057d3799ae4cc34
Sathish Perumal <sathishperumalmsc@gmail.com>
2024-08-30 02:05:27 +0530

    modified .json files

--- GetPage.cs
+++ GetPage.cs
@@ -1,19 +1,28 @@
 ﻿using Microsoft.Playwright;
-using Automation.SwagLabsNET.Pom;
-using Automation.SwagLabsNET.Extensions;
-using static System.Net.Mime.MediaTypeNames;
-
 using Automation.SwagLabsNET.Extensions.PlaywrightExtensions;
 using Automation.SwagLabsNET.Pom.Constants;
+using Microsoft.Extensions.Configuration;
 
 namespace Automation.SwagLabsNET.Test
 {
     public static class GetPage
     {
-        private static IPage page;
+        private static IPage? page;
+        private static IConfiguration? _config;
 
         public static async Task<IPage> GetWebPage() 
-        { 
+        {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
+          
+            _config = new ConfigurationBuilder()
+                .AddJsonFile($"appsettings.{environment}.json", reloadOnChange: true, optional: true)
+                .Build();
+
+            await Console.Out.WriteLineAsync($"environment: {environment}");
+
+            Console.WriteLine(_config["Appsettings:username"]);
+            Console.WriteLine(_config["Appsettings:password"]);
+
             var playwright = await Playwright.CreateAsync();
             var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Channel = "chrome", Headless=false});
             page = await browser.NewPageAsync();
